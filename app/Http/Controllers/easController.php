d689501505c91784b6a841e7186df5c476951d17
modalhoki <aria.satyamahatma@gmail.com>
2021-01-12 08:37:23 +0700

    KODE SOAL 04

--- app/Http/Controllers/easController.php
+++ app/Http/Controllers/easController.php
@@ -27,9 +27,7 @@ class easController extends Controller
     }
 
     public function delete($nrp) {
-        // menghapus data pegawai berdasarkan id yang dipilih
             DB::table('mahasiswa')->where('NRP',$nrp)->delete();
-            // alihkan halaman ke halaman pegawai
             return redirect('eas');
         }
 
